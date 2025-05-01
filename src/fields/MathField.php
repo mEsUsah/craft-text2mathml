f5e324149ba0666fb10babb51b3187afa50a44b8
Stanley Skarshaug <stanley@haxor.no>
2025-05-01 22:12:30 +0200

    fix: make normal saves work

--- src/fields/MathField.php
+++ src/fields/MathField.php
@@ -66,11 +66,12 @@ class MathField extends Field
     protected function inputHtml(mixed $value, ?ElementInterface $element, bool $inline): string
     {
         $fieldData = json_decode($value);
-        
+
         // Render the HTML for the field
         return Craft::$app->getView()->renderTemplate('text2mathml/fields/mathField', [
             'field' => $this,
             'input' => $fieldData->input ?? '',
+            'output' => $fieldData->output ?? '',
             'valueDecoded' => $fieldData,
         ]);
     }
@@ -80,13 +81,62 @@ class MathField extends Field
         // Update the output value before saving to the database
         if ($element->isFieldDirty($this->handle)) {
             $value = $element->getFieldValue($this->handle);
+
+            $mathml = $this->getMathML($value);
+            $mathml = str_replace('<pre>', '', $mathml);
+            $mathml = str_replace('</pre>', '', $mathml);
+            $mathml = str_replace('&lt;', '<', $mathml);
+            $mathml = str_replace('&gt;', '>', $mathml);
+            $mathml = str_replace('&gt;', '>', $mathml);
+            $mathml = str_replace('&#39;', "'",$mathml);
+            $mathml = str_replace('\n', '',$mathml);
+
             $valueJson = json_encode([
                 'input' => $value,
-                'output' => "test",
+                'output' => $mathml,
             ]);
             $element->setFieldValue($this->handle, $valueJson);
         }
         return parent::beforeElementSave($element, $isNew);
+    }
+
+    private function getMathML(string $input): string
+    {
+        // contact the MathML API to convert the input to MathML
+        try {
+            $data = [
+                'tst1' => '{"delivery" ->"embed","character" ->"mathml","indent" ->"false","markup" ->"presentation","declare" ->"false","document" ->"false"}',
+                'formattype1' => 'MathML',
+                'txt' => $input,
+                'type' => 'txt',
+                'formname' => 'tomathml',
+                'form' => 'TraditionalForm',
+            ];
+            
+            $ch = curl_init('https://www.mathmlcentral.com/Tools/XhtmlResult.jsp');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/x-www-form-urlencoded',
+                'Accept: text/html, */*',
+                'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+                'x-requested-with: XMLHttpRequest',
+                'host: www.mathmlcentral.com',
+                'referer: https://www.mathmlcentral.com/Tools/ToMathML.jsp',
+            ]);
+            
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            if ($response === false) {
+                throw new \Exception('Failed to fetch MathML from the API');
+            }
+            return $response;
+        } catch (\Exception $e) {
+            Craft::error('Error fetching MathML: ' . $e->getMessage(), __METHOD__);
+            return '<p>API error</p>';
+        }
     }
 
     public function getElementValidationRules(): array
